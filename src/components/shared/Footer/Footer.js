5de4c47ab71c876eea961caf553cbd4c108d86cd
Amirul Islam Hridoy <hossainhridoy30687@gmail.com>
2022-10-07 19:52:34 +0600

    footer responsive

--- src/components/shared/Footer/Footer.js
+++ src/components/shared/Footer/Footer.js
@@ -3,10 +3,10 @@ import React from 'react';
 const Footer = () => {
     return (
         <footer className='bg-[#5D10E3]'>
-            <div className='pt-[51px] pb-[66px] max-w-7xl mx-auto'>
-                <div className='grid grid-cols-5'>
+            <div className='pt-[51px] pb-[66px] px-5 2xl:px-0 max-w-7xl mx-auto'>
+                <div className='sm:grid grid-cols-3 lg:grid-cols-5'>
                     <div className='flex flex-col'>
-                        <h4 className='text-[#F53289] font-bold text-[22px] mb-[27px]'>Product</h4>
+                        <h4 className='text-[#F53289] font-bold text-[22px] mb-1.5 lg:mb-[27px]'>Product</h4>
                         <a className='text-white text-[14px] my-1' href=''>Landing Page</a>
                         <a className='text-white text-[14px] my-1' href=''>Popup Builder</a>
                         <a className='text-white text-[14px] my-1' href=''>Web-design</a>
@@ -14,14 +14,14 @@ const Footer = () => {
                         <a className='text-white text-[14px] my-1' href=''>Integrations</a>
                     </div>
                     <div className='flex flex-col'>
-                        <h4 className='text-[#F53289] font-bold text-[22px] mb-[27px]'>Use Cases</h4>
+                        <h4 className='text-[#F53289] font-bold text-[22px] mb-1.5 lg:mb-[27px] mt-4 sm:mt-0'>Use Cases</h4>
                         <a className='text-white text-[14px] my-1' href=''>Web-designers</a>
                         <a className='text-white text-[14px] my-1' href=''>Marketers</a>
                         <a className='text-white text-[14px] my-1' href=''>Small Business</a>
                         <a className='text-white text-[14px] my-1' href=''>Website Builder</a>
                     </div>
                     <div className='flex flex-col'>
-                        <h4 className='text-[#F53289] font-bold text-[22px] mb-[27px]'>Resources</h4>
+                        <h4 className='text-[#F53289] font-bold text-[22px] mb-1.5 lg:mb-[27px] mt-4 sm:mt-0'>Resources</h4>
                         <a className='text-white text-[14px] my-1' href=''>Academy</a>
                         <a className='text-white text-[14px] my-1' href=''>Blog</a>
                         <a className='text-white text-[14px] my-1' href=''>Themes</a>
@@ -29,16 +29,16 @@ const Footer = () => {
                         <a className='text-white font-[14px] my-1' href=''>Developers</a>
                         <a className='text-white text-[14px] my-1' href=''>Support</a>
                     </div>
-                    <div className='col-start-4 col-end-6 flex flex-col'>
-                        <h4 className='text-[#F53289] font-bold text-[22px] mb-[27px]'>Subscribe</h4>
+                    <div className='col-span-full lg:col-start-4 lg:col-end-6 flex flex-col'>
+                        <h4 className='text-[#F53289] font-bold text-[22px] mb-1.5 lg:mb-[27px] mt-4 sm:mt-[28px] lg:mt-0'>Subscribe</h4>
                         <p className='text-white text-base mb-4'>Subscribe to stay tuned for new web-design and latest updates. Lets do it!</p>
-                        <form className='flex gap-2.5'>
-                            <input className='placeholder-black font-normal text-base py-[18px] px-[10px] rounded-lg w-[290px] outline-white' type='text' placeholder='Enter your email Address'></input>
-                            <input className='text-white bg-[#F53289] font-normal rounded-lg w-[132px] px-[18px] py-[18px] cursor-pointer' type='submit' value='Subscribe'></input>
+                        <form className='flex flex-col sm:flex-row gap-2.5'>
+                            <input className='placeholder-black font-normal text-base py-2.5 sm:py-[18px] px-[10px] rounded-lg max-w-[350px] sm:min-w-[290px] lg:w-[290px] outline-white' type='text' placeholder='Enter your email Address'></input>
+                            <input className='text-white bg-[#F53289] font-normal rounded-lg w-[132px] px-[18px] py-2.5 sm:py-[18px] cursor-pointer' type='submit' value='Subscribe'></input>
                         </form>
                     </div>
                 </div>
-                <p className='text-white text-[12px] mt-[60px]'>&#169; 2022 All Rights Resierved</p>
+                <p className='text-white text-[12px] mt-5 lg:mt-[60px]'>&#169; 2022 All Rights Resierved</p>
             </div>
         </footer>
     );
