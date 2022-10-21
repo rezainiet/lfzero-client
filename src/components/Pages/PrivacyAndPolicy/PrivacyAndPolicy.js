9c31741eb7e5f68cd535505623354ac7dc7ddc9e
Shakilk Sarder <shakil01748@hotmail.com>
2022-10-22 01:04:02 +0600

    Notfound-Footerpage

---
+++ src/components/Pages/PrivacyAndPolicy/PrivacyAndPolicy.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const PrivacyAndPolicy = () => {
+    return (
+        <div>
+            <h1 className=" text-black  px-28 ml- mt-8">
+                <span className="text-4xl ">
+                    Privacy And Policy <br />
+                    <span className="font-bold text-2xl"> Learn From Zero</span>
+                </span>
+                <br /> <br />
+                We take the responsibility to respect your private information online. We further acknowledge the need to preserve and handle the information you share with us that is identifiable personally ("personal information"). Information which we regard as personal about you includes but is not confined to your name, address, e-mail address, phone number or other contact details. You must provide the following information, but it is not limited to: phone number; bank information; date of birth; class, school, subjects and syllabus; e-mail address in order to buy a product from our website or application. This Privacy Policy also applies to data collected from users who are not recorded as members, including but not limited to surfing behavior, pages viewed etc. In addition, the Website and App requests permits to link your LFZ Account to your Social Account, and if you have provided permission to link the Accounts, the information shall be gathered straight from your social media account.
+            </h1>
+
+        </div>
+    );
+};
+
+export default PrivacyAndPolicy;
