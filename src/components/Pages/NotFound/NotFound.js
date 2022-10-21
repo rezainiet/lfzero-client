bd17874cbe3a96e5bd0debd5c98c1d822f16d63b
Masud Reza <81326757+rezainiet@users.noreply.github.com>
2022-10-22 01:17:03 +0600

    centered 404 page

--- src/components/Pages/NotFound/NotFound.js
+++ src/components/Pages/NotFound/NotFound.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import notfound from '../../../assets/images/notfound.gif'
 const NotFound = () => {
     return (
-        <div>
+        <div className='flex items-center justify-center'>
             <Link to='/'>   <img src={notfound} alt="" /></Link>
         </div>
     );
