2edd8445bd49056e4fd7ab2536f65b1d9f6518c6
Amirul Islam Hridoy <hossainhridoy30687@gmail.com>
2022-10-24 10:59:27 +0600

    responsibr navbar

--- src/components/shared/Navbar/Navbar.js
+++ src/components/shared/Navbar/Navbar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useInsertionEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import auth from "../../../firebase.init";
 import { useAuthState } from "react-firebase-hooks/auth";
@@ -8,106 +8,106 @@ import { signOut } from "firebase/auth";
 const Navbar = () => {
   const [user, loading] = useAuthState(auth);
   const navigate = useNavigate();
+  const [check, setCheck] = useState(false)
 
   return (
-    <div className="bg-[#5D10E3] px-20">
-      <div className="flex justify-between items-center p-6 px-6 lg:px-0 container mx-auto">
+    <div className="bg-[#5D10E3] sm:px-20">
+      <div className="flex justify-between items-center p-6 px-6 lg:px-0 container mx-auto ">
         {/* Logo */}
-        <div
-          className="text-lg font-bold uppercase text-white"
-          onClick={() => navigate("/")}
-        >
-          <img src={logo} alt="logo" width={75} />
-        </div>
+        <div className="flex justify-between w-full xl:w-[150px]">
+          <div
+            className="text-lg font-bold uppercase text-white cursor-pointer"
+            onClick={() => navigate("/")}>
+            <img src={logo} alt="logo" width={75} />
+          </div>
 
-        {/* Hamburger */}
-        <input
-          className="peer hidden"
-          type="checkbox"
-          name="hamburger"
-          id="hamburger"
-        />
-        <label
-          htmlFor="hamburger"
-          className="peer-checked:hamburger block relative cursor-pointer lg:hidden border-2 border-gray-300 peer-checked:border-2
-                 peer-checked:border-white p-3 rounded-md transition-all"
-        >
-          <div className="m-auto w-6 h-1 rounded bg-gray-300 transition-all duration-300"></div>
-          <div className="m-auto mt-2 w-6 h-1 rounded bg-gray-300 transition-all duration-300"></div>
-        </label>
+          {/* Hamburger */}
+          <input
+            className="peer hidden"
+            type="checkbox"
+            name="hamburger"
+            id="hamburger"
+            onChange={(e) => setCheck(e.target.checked)}
+          />
+          <label htmlFor="hamburger"
+            className="peer-checked:hamburger block relative cursor-pointer lg:hidden border-2 border-gray-300 peer-checked:border-2
+                 peer-checked:border-white p-1 rounded-md transition-all">
+            {check ? <i class="fa-solid fa-x text-white font-bold px-1 text-xl"></i>
+              :
+              <>
+                {/* <div className="m-auto w-4 sm:w-6 h-1 rounded bg-gray-300 transition-all duration-300"></div>
+                <div className="m-auto mt-2 w-4 sm:w-6 h-1 rounded bg-gray-300 transition-all duration-300"></div> */}
+                <i class="fa-solid fa-bars text-white font-bold px-1 text-xl"></i>
+              </>}
+          </label>
+        </div>
 
         {/* Menu navbar */}
-        <div
-          className="-translate-y-full lg:-ml-20 peer-checked:translate-y-0 lg:translate-y-0 inset-0 fixed lg:static pt-20 lg:pt-0 bg-white lg:bg-transparent
-                 -z-10 lg:z-10 lg:h-auto lg:w-auto transition-all duration-300 ease-in-out"
-        >
+        <div className={`${check ? "top-[80px]" : "top-[-280px]"} flex-1 flex absolute lg:static z-50 lg:z-0 bg-gradient-to-b from-[#5119CD] to-blue-600 lg:bg-gradient-to-b lg:from-[#5d10e3] lg:to-[#5d10e3] w-full left-0 lg:justify-between lg:items-center flex-col lg:flex-row justify-center items-center text-center duration-700`}>
           <div
-            className="bg-white text-white shadow-md lg:bg-transparent lg:shadow-none py-10 lg:py-0 flex flex-col lg:items-center lg:flex-row px-6 space-y-4 
-                    lg:space-y-0 lg:space-x-3 "
-          >
-            <a href="#">Category</a>
-            <a href="#">Organization</a>
-            <a href="#">Courses</a>
+            className="">
+            <div
+              className="text-white lg:bg-transparent flex flex-col lg:items-center lg:flex-row lg:px-6 space-y-2 pt-3 lg:pt-0 lg:space-y-0 lg:space-x-3">
+              <a href="#">Category</a>
+              <a href="#">Organization</a>
+              <a href="#">Courses</a>
+            </div>
+          </div>
+          <div
+            className="">
+            <div
+              className="lg:bg-transparent py-2 lg:py-10 lg:py-0 flex flex-col lg:items-center lg:flex-row px-6 space-y-2 
+                    lg:space-y-0 lg:space-x-3">
+              <input
+                type="text"
+                className="outline-none py-1 px-5 w-72 sm:w-96 lg:w-60 xl:w-96 rounded-xl focus:ring-1 focus:ring-pink-500"
+                placeholder="Search"
+              />
+            </div>
+          </div>
+          <div
+            className="">
+            <div
+              className="lg:bg-transparent pt-2 pb-5 lg:py-10 lg:py-0 flex flex-col lg:items-center lg:flex-row px-6 space-y-4 
+                    lg:space-y-0 lg:space-x-3">
+              {/* login */}
+              {user ? (
+                <>
+                  {" "}
+                  <button
+                    onClick={() => signOut(auth)}
+                    className="bg-white text-[#F53289] px-7 py-2 rounded-full"
+                  >
+                    Logout
+                  </button>{" "}
+                </>
+              ) : (
+                <>
+                  <button
+                    onClick={() => navigate("/login")}
+                    className="bg-white text-[#F53289] px-7 py-2 rounded-full"
+                  >
+                    Login
+                  </button>
+                  <button
+                    onClick={() => navigate("/signUp")}
+                    className="bg-[#F53289] text-white px-7 py-2 rounded-full"
+                  >
+                    Registration
+                  </button>
+                </>
+              )}
+            </div>
           </div>
         </div>
-        <div
-          className="-translate-y-full peer-checked:translate-y-0 lg:translate-y-0 inset-0 fixed lg:static pt-20 lg:pt-0 bg-white lg:bg-transparent
-                 -z-10 lg:z-10 lg:h-auto lg:w-auto transition-all duration-300 ease-in-out"
-        >
-          <div
-            className="bg-white shadow-md lg:bg-transparent lg:shadow-none py-10 lg:py-0 flex flex-col lg:items-center lg:flex-row px-6 space-y-4 
-                    lg:space-y-0 lg:space-x-3"
-          >
-            <input
-              type="text"
-              className="outline-none py-1 px-5 w-96 rounded-xl focus:ring-1 focus:ring-pink-500"
-              placeholder="Search"
-            />
-          </div>
-        </div>
-        <div
-          className="-translate-y-full peer-checked:translate-y-0 lg:translate-y-0 inset-0 fixed lg:static pt-20 lg:pt-0 bg-white lg:bg-transparent
-                 -z-10 lg:z-10 lg:h-auto lg:w-auto transition-all duration-300 ease-in-out"
-        >
-          <div
-            className="bg-white shadow-md lg:bg-transparent lg:shadow-none py-10 lg:py-0 flex flex-col lg:items-center lg:flex-row px-6 space-y-4 
-                    lg:space-y-0 lg:space-x-3"
-          >
-            {user ? (
-              <>
-                {" "}
-                <button
-                  onClick={() => signOut(auth)}
-                  className="bg-white text-[#F53289] px-7 py-2 rounded-full"
-                >
-                  Logout
-                </button>{" "}
-              </>
-            ) : (
-              <>
-                <button
-                  onClick={() => navigate("/login")}
-                  className="bg-white text-[#F53289] px-7 py-2 rounded-full"
-                >
-                  Login
-                </button>
-                <button
-                  onClick={() => navigate("/signUp")}
-                  className="bg-[#F53289] text-white px-7 py-2 rounded-full"
-                >
-                  Registration
-                </button>
-              </>
-            )}
-          </div>
-        </div>
+        {/* user login */}
         {user ? (
           <>
             <div className="dropdown dropdown-end">
-              <label tabIndex={0} className="btn m-1 border-none">
+              <label tabIndex={0} className="btn ml-2 border-none p-0">
                 <img
                   src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQt-F5GQg8qB2fWquF1ltQvAT2Z8Dv5pJLb9w&usqp=CAU"
-                  width="40px"
+                  className='w-[44px] lg:min-w-[40px] lg:w-[200px] xl:w-[40px] rounded'
                   alt=""
                 ></img>
               </label>
