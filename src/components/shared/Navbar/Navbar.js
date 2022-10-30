393513af319794a45376d97f57b79b8050c70ca1
rezainiet <masudrezaog@Gmail.com>
2022-10-30 18:43:18 +0600

    removed link from import

--- src/components/shared/Navbar/Navbar.js
+++ src/components/shared/Navbar/Navbar.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useInsertionEffect, useState } from "react";
-import { Link, Link, useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import auth from "../../../firebase.init";
 import { useAuthState } from "react-firebase-hooks/auth";
 import logo from "./../../../assets/images/icons/logo.png";
